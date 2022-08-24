8af2120401cfb3265e910c5163fdd5eabc5a7c52
Jin Byeon <jinsoobyeon@gmail.com>
2022-08-24 17:45:14 +0900

    Read to-do list from text file

--- src/client/ClientClass.java
+++ src/client/ClientClass.java
@@ -1,7 +1,9 @@
 package client;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Scanner;
@@ -9,13 +11,18 @@ import java.util.Scanner;
 public class ClientClass {
 	public static void main(String[] args) {
 		String todo;
+		String todoList;
 		Scanner scanner;
 		Socket socket;
+		InputStream inputStream;
+		DataInputStream dataInputStream;
 		OutputStream outputStream;
 		DataOutputStream dataOutputStream;
 		
 		try {
 			socket = new Socket("localhost", 9000);
+			inputStream = socket.getInputStream();
+			dataInputStream = new DataInputStream(inputStream);
 			outputStream = socket.getOutputStream();
 			dataOutputStream = new DataOutputStream(outputStream);
 			scanner = new Scanner(System.in);
@@ -25,6 +32,9 @@ public class ClientClass {
 				todo = scanner.nextLine();
 				dataOutputStream.writeUTF(todo);
 				dataOutputStream.flush();
+				
+				todoList = dataInputStream.readUTF();
+				System.out.println(todoList);
 			}
 		} catch(IOException e) {
 			e.printStackTrace();
--- src/server/ServerClass.java
+++ src/server/ServerClass.java
@@ -2,9 +2,11 @@ package server;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -13,13 +15,18 @@ import java.util.Arrays;
 public class ServerClass {
 	public static void main(String[] args) {
 		String todo;
+		String todoList;
 		ArrayList<String> todos;
 		Socket socket;
 		ServerSocket serverSocket;
 		InputStream inputStream;
 		DataInputStream dataInputStream;
-		FileOutputStream fileOutputStream;
+		OutputStream outputStream;
 		DataOutputStream dataOutputStream;
+		FileInputStream fileInputStream;
+		DataInputStream fileDataInputStream;
+		FileOutputStream fileOutputStream;
+		DataOutputStream fileDataOutputStream;
 		
 		try {
 			todos = new ArrayList<String>();
@@ -27,17 +34,28 @@ public class ServerClass {
 			socket = serverSocket.accept();
 			inputStream = socket.getInputStream();
 			dataInputStream = new DataInputStream(inputStream);
+			outputStream = socket.getOutputStream();
+			dataOutputStream = new DataOutputStream(outputStream);
 			
 			while (true) {
 				todo = dataInputStream.readUTF();
 				todos.add(todo);
 				
 				fileOutputStream = new FileOutputStream("./src/database/todos.txt");
-				dataOutputStream = new DataOutputStream(fileOutputStream);
-				dataOutputStream.writeChars(Arrays.toString(todos.toArray()));
-				dataOutputStream.flush();
-				dataOutputStream.close();
+				fileDataOutputStream = new DataOutputStream(fileOutputStream);
+				fileDataOutputStream.writeUTF(Arrays.toString(todos.toArray()));
+				fileDataOutputStream.flush();
+				fileDataOutputStream.close();
 				fileOutputStream.close();
+				
+				fileInputStream = new FileInputStream("./src/database/todos.txt");
+				fileDataInputStream = new DataInputStream(fileInputStream);
+				todoList = fileDataInputStream.readUTF();
+				fileDataInputStream.close();
+				fileInputStream.close();
+				
+				dataOutputStream.writeUTF(todoList);
+				dataOutputStream.flush();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
